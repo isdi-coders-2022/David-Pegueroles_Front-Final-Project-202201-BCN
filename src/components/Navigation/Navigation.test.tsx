776acf3eba2ed1a661bcc9ed9025d230c1cc6120
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-03-16 21:12:27 +0100

    Added test of Navigation component

---
+++ src/components/Navigation/Navigation.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../../theme";
+import Navigation from "./Navigation";
+
+describe("Given a Navigation component", () => {
+  describe("When it's rendered", () => {
+    test("Then it should display links 'USER', 'CALENDAR', 'MAPS' and 'APPOINTMENTS'", () => {
+      render(
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <Navigation />
+          </ThemeProvider>
+        </BrowserRouter>
+      );
+
+      const user = screen.getByRole("link", { name: /user/i });
+      const calendar = screen.getByRole("link", { name: /calendar/i });
+      const map = screen.getByRole("link", { name: /map/i });
+      const appointment = screen.getByRole("link", { name: /appointment/i });
+
+      expect(user).toBeInTheDocument();
+      expect(calendar).toBeInTheDocument();
+      expect(map).toBeInTheDocument();
+      expect(appointment).toBeInTheDocument();
+    });
+  });
+});
