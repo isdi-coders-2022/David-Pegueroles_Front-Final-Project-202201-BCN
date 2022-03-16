63a830855ecadd5864b7c9c9a48c2989f0550f1b
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-03-16 20:42:58 +0100

    Added Navigation component in the App return

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Navigation from "./components/Navigation/Navigation";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navigation />
+      <h1>Site under construction, please come later</h1>
+    </>
+  );
 }
 
 export default App;
