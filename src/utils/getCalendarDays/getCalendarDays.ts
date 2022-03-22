3b2df6a4b92131d984ed6481878ad2ff0ba958f3
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-03-23 00:49:42 +0100

    Changed format of the returned date

--- src/utils/getCalendarDays/getCalendarDays.ts
+++ src/utils/getCalendarDays/getCalendarDays.ts
@@ -1,16 +1,18 @@
+import { format } from "date-fns";
+
 const getCalendarDays = (
   month: number = new Date().getMonth(),
   year: number = new Date().getFullYear()
 ): string[][] => {
   const monthFirstDayPosition: number = new Date(year, month, 1).getDay();
 
-  let calendarDay: number = 1 - (monthFirstDayPosition - 1);
+  let calendarDay: number = 1 - monthFirstDayPosition;
 
   const calendarDays: string[][] = new Array(5).fill([]).map((): string[] => {
     return new Array(7).fill(null).map((): string => {
       calendarDay++;
-      const day = new Date(year, month, calendarDay);
-      return day.toISOString();
+      const day = format(new Date(year, month, calendarDay), "do-MM-yyyy");
+      return day;
     });
   });
 
