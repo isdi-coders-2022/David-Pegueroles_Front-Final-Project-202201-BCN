487cd7b8f6a7811e8607128a2da0b9852b3ba65b
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-03-27 12:45:39 +0200

    Updated the getCalendarDays function and its test

--- src/utils/getCalendarDays/getCalendarDays.ts
+++ src/utils/getCalendarDays/getCalendarDays.ts
@@ -1,20 +1,24 @@
-import { format } from "date-fns";
+import { format, getWeeksInMonth } from "date-fns";
 
 const getCalendarDays = (
   month: number = new Date().getMonth(),
   year: number = new Date().getFullYear()
 ): string[][] => {
-  const monthFirstDayPosition: number = new Date(year, month, 1).getDay();
+  const date = new Date(year, month, 1);
+  const monthFirstDayPosition: number = date.getDay();
+  const numberOfWeeksToShow: number = getWeeksInMonth(date);
 
   let calendarDay: number = 1 - monthFirstDayPosition;
 
-  const calendarDays: string[][] = new Array(5).fill([]).map((): string[] => {
-    return new Array(7).fill(null).map((): string => {
-      calendarDay++;
-      const day = format(new Date(year, month, calendarDay), "do-MM-yyyy");
-      return day;
+  const calendarDays: string[][] = new Array(numberOfWeeksToShow)
+    .fill([])
+    .map((): string[] => {
+      return new Array(7).fill(null).map((): string => {
+        calendarDay++;
+        const day = format(new Date(year, month, calendarDay), "d-MM-yyyy");
+        return day;
+      });
     });
-  });
 
   return calendarDays;
 };
