d0b26c67a1bf7f1bf622e0f1eee1042e63a8921f
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-03-28 12:52:19 +0200

    Added test of App to test the routes

---
+++ src/App.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { theme } from "./theme";
+
+describe("Given an App component", () => {
+  describe("When it's rendered and the user clicks at the calendar link", () => {
+    test("Then it should navigate to the calendar page", () => {
+      const linkText = /calendar/i;
+      const path = "/map";
+      const expectedPath = "/calendar";
+
+      const history = createMemoryHistory({ initialEntries: [path] });
+
+      render(
+        <Router location={history.location} navigator={history}>
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
+        </Router>
+      );
+
+      expect(history.location.pathname).toBe(path);
+
+      const link = screen.getByRole("link", { name: linkText });
+
+      userEvent.click(link);
+
+      expect(history.location.pathname).toBe(expectedPath);
+    });
+  });
+});
