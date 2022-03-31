702977b9016d57e53debd843e0a3cc60139f9311
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-03-31 10:50:58 +0200

    Added style to SelectedDay component

---
+++ src/components/SelectedDay/SelectedDay.style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Title = styled.h2`
+  font-family: "Lexend Deca";
+  margin: 0;
+  margin-left: 20px;
+  margin-bottom: 30px;
+  padding: 0;
+`;
+
+export default Title;
