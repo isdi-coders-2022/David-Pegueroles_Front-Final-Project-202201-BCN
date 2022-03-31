d048cbc8d3cb27c10a93353be06df7e26e97beaf
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-03-31 12:24:03 +0200

    Updated test description to be more accurate

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -3,9 +3,9 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "../../theme";
 import Header from "./Header";
 
-describe("Given a Navigation component", () => {
+describe("Given a Header component", () => {
   describe("When it's rendered", () => {
-    test("Then it should display links 'USER', 'CALENDAR', 'MAPS' and 'APPOINTMENTS'", () => {
+    test("Then it should display an image with the 'Pin Your Meetings Logo'", () => {
       const logoAcessibleText = /pin your meetings logo/i;
 
       render(
