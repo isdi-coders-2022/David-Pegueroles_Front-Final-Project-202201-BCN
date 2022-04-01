7e5f28bf719f69f9d554d39c449224d3e8f92508
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-04-01 03:40:58 +0200

    Added test of the actionOnClick

--- src/components/AppointmentBanner/AppointmentBanner.test.tsx
+++ src/components/AppointmentBanner/AppointmentBanner.test.tsx
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../../theme";
 import AppointmentBanner from "./AppointmentBanner";
 
 describe("Given an AppointmentBanner component", () => {
-  describe("When it's rendered with an appointment", () => {
+  describe("When it's rendered with an appointment and actionOnClick", () => {
     test("Then it should display the name & hour of the appointment, and an edit image", () => {
       const name = /do something/i;
       const hour = /12:00/i;
       const edit = /edit/i;
+      const actionOnClick = jest.fn();
 
       const appointment = {
         name: "Do something",
@@ -22,7 +24,10 @@ describe("Given an AppointmentBanner component", () => {
 
       render(
         <ThemeProvider theme={theme}>
-          <AppointmentBanner appointment={appointment} />
+          <AppointmentBanner
+            appointment={appointment}
+            actionOnClick={() => actionOnClick()}
+          />
         </ThemeProvider>
       );
 
@@ -33,6 +38,38 @@ describe("Given an AppointmentBanner component", () => {
       expect(nameListItem).toBeInTheDocument();
       expect(hourListItem).toBeInTheDocument();
       expect(editImage).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's rendered with an appointment & actionOnClick and the user clicks at the appointment", () => {
+    test("Then it should call the actionOnClick", () => {
+      const name = /do something/i;
+      const actionOnClick = jest.fn();
+
+      const appointment = {
+        name: "Do something",
+        description: "This is another thing to do",
+        date: "2022-03-31",
+        category: "Work",
+        location: "C/ Diputació 37, Barcelona",
+        hour: "12:00",
+        id: "624212b09666edf108d06d6a",
+      };
+
+      render(
+        <ThemeProvider theme={theme}>
+          <AppointmentBanner
+            appointment={appointment}
+            actionOnClick={() => actionOnClick()}
+          />
+        </ThemeProvider>
+      );
+
+      const nameListItem = screen.getByRole("listitem", { name: name });
+
+      userEvent.click(nameListItem);
+
+      expect(actionOnClick).toHaveBeenCalled();
     });
   });
 });
