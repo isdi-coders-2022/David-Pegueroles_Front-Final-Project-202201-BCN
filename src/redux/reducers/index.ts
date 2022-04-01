887d60abdc7b90011bf29f3fec1f2cdeb3bf19c5
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-04-01 03:10:18 +0200

    Added appointmentInfoReducer to the rootReducer as appointmentInfo

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
+import appointmentInfoReducer from "./appointmentInfoReducer";
 import dailyAppointmentsReducer from "./dailyAppointmentsReducer";
 import navigationPositionReducer from "./navigationPositionReducer";
 
 const rootReducer = combineReducers({
   dailyAppointments: dailyAppointmentsReducer,
   navigationPosition: navigationPositionReducer,
+  appointmentInfo: appointmentInfoReducer,
 });
 
 export default rootReducer;
