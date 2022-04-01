c0aaaf5144b067a850be410f01fb463073267523
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-04-01 03:24:05 +0200

    Added test of loadAppointmentInfoThunk function

--- src/redux/thunks/appointmentsThunks.test.ts
+++ src/redux/thunks/appointmentsThunks.test.ts
@@ -1,9 +1,12 @@
 import { Appointment } from "../../types/Appointment";
 import actionsTypes from "../actions/actionsTypes";
-import { loadDailyAppointmentsThunk } from "./appointmentsThunks";
+import {
+  loadAppointmentInfoThunk,
+  loadDailyAppointmentsThunk,
+} from "./appointmentsThunks";
 
 describe("Given a loadDailyAppointmentsThunk function", () => {
-  describe("When it's invoked", () => {
+  describe("When it's invoked with '29-03-2022'", () => {
     test("Then it should call the dispatch function", async () => {
       const date = "29-03-2022";
 
@@ -34,6 +37,35 @@ describe("Given a loadDailyAppointmentsThunk function", () => {
 
       const dispatch = jest.fn();
       const dailyAppointmentsThunk = loadDailyAppointmentsThunk(date);
+
+      await dailyAppointmentsThunk(dispatch);
+
+      expect(dispatch).toHaveBeenCalledWith(action);
+    });
+  });
+});
+
+describe("Given a loadAppointmentInfoThunk function", () => {
+  describe("When it's invoked with the id '624210049666edf108d06d69'", () => {
+    test("Then it should call the dispatch function", async () => {
+      const id = "624210049666edf108d06d69";
+
+      const appointment: Appointment = {
+        name: "Do something",
+        description: "This should do",
+        date: "2022-03-29",
+        category: "Work",
+        location: "C/ Diputació 37, Barcelona",
+        hour: "10:00",
+        id: "624210049666edf108d06d69",
+      };
+      const action = {
+        type: actionsTypes.loadAppointmentInfo,
+        appointment,
+      };
+
+      const dispatch = jest.fn();
+      const dailyAppointmentsThunk = loadAppointmentInfoThunk(id);
 
       await dailyAppointmentsThunk(dispatch);
 
