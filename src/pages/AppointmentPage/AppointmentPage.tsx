6c35367139dc16efc9e3281a9528e46e79a7b030
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-04-01 08:14:36 +0200

    Added a deleteOnClick function that is passed to AppointmentDisplay component via props

--- src/pages/AppointmentPage/AppointmentPage.tsx
+++ src/pages/AppointmentPage/AppointmentPage.tsx
@@ -1,18 +1,28 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import AppointmentDisplay from "../../components/AppointmentDisplay/AppointmentDisplay";
 import { navigationPositionAction } from "../../redux/actions/actionsCreators";
 import { RootState } from "../../redux/store";
-import { loadAppointmentInfoThunk } from "../../redux/thunks/appointmentsThunks";
+import {
+  deleteAppointmentThunk,
+  loadAppointmentInfoThunk,
+} from "../../redux/thunks/appointmentsThunks";
 import { Appointment } from "../../types/Appointment";
 import Heading from "./AppointmentPage.style";
 
 const AppointmentPage = (): JSX.Element => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const appointmentInfo: Appointment = useSelector(
     (state: RootState) => state.appointmentInfo
   );
+
+  const deleteOnClick = async (id: string | undefined) => {
+    await dispatch(deleteAppointmentThunk(id));
+    navigate("/calendar");
+  };
 
   useEffect(() => {
     const thisAppointmentId = window.location.pathname.split("/", 4);
@@ -24,7 +34,10 @@ const AppointmentPage = (): JSX.Element => {
     <>
       <Heading>Appointment</Heading>
 
-      <AppointmentDisplay appointment={appointmentInfo} />
+      <AppointmentDisplay
+        appointment={appointmentInfo}
+        deleteOnClick={deleteOnClick}
+      />
     </>
   );
 };
