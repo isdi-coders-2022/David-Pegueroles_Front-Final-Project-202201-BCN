42fd9ecf34d89f66aae8dd43b2268bf552601cad
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-04-01 08:13:49 +0200

    Added test of the deleteOnClick function

--- src/components/AppointmentDisplay/AppointmentDisplay.test.tsx
+++ src/components/AppointmentDisplay/AppointmentDisplay.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../../theme";
 import AppointmentDisplay from "./AppointmentDisplay";
@@ -17,10 +18,14 @@ describe("Given an AppointmentDisplay component", () => {
         hour: "10:00",
         id: "624210049666edf108d06d69",
       };
+      const deleteOnClick = jest.fn();
 
       render(
         <ThemeProvider theme={theme}>
-          <AppointmentDisplay appointment={appointmentInfo} />
+          <AppointmentDisplay
+            appointment={appointmentInfo}
+            deleteOnClick={deleteOnClick}
+          />
         </ThemeProvider>
       );
       const heading = screen.getByRole("heading", { name: appointmentName });
@@ -42,10 +47,14 @@ describe("Given an AppointmentDisplay component", () => {
         hour: "10:00",
         id: "624210049666edf108d06d69",
       };
+      const deleteOnClick = jest.fn();
 
       render(
         <ThemeProvider theme={theme}>
-          <AppointmentDisplay appointment={appointmentInfo} />
+          <AppointmentDisplay
+            appointment={appointmentInfo}
+            deleteOnClick={deleteOnClick}
+          />
         </ThemeProvider>
       );
 
@@ -58,6 +67,37 @@ describe("Given an AppointmentDisplay component", () => {
       expect(receivedDate).toBeInTheDocument();
       expect(receivedHour).toBeInTheDocument();
       expect(receivedDescription).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's rendered with an appointment & a deleteOnClick function, & the user clicks at the delete list item", () => {
+    test("Then the deleteOnClick function should be called", () => {
+      const deleteName = /delete/i;
+
+      const appointmentInfo = {
+        name: "Do something",
+        description: "This should do",
+        date: "2022-03-29",
+        category: "Work",
+        location: "C/ Diputació 37, Barcelona",
+        hour: "10:00",
+        id: "624210049666edf108d06d69",
+      };
+      const deleteOnClick = jest.fn();
+
+      render(
+        <ThemeProvider theme={theme}>
+          <AppointmentDisplay
+            appointment={appointmentInfo}
+            deleteOnClick={deleteOnClick}
+          />
+        </ThemeProvider>
+      );
+      const deleteItem = screen.getByRole("listitem", { name: deleteName });
+
+      userEvent.click(deleteItem);
+
+      expect(deleteOnClick).toHaveBeenCalled();
     });
   });
 });
